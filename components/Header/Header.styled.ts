3fb6c4be9d43e3cfd5e9f21046326454ce5c5cc3
Trần Hữu Thanh <88789960+IT0505@users.noreply.github.com>
2022-05-09 10:50:02 +0700

    Update container config

--- components/Header/Header.styled.ts
+++ components/Header/Header.styled.ts
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 import tw from "twin.macro";
 
+export const Header = styled.div`
+  ${tw``}
+`
+
 export const FooterContainer = styled.div`
   ${tw`container mx-auto px-4 py-8  flex flex-col lg:flex-row flex-wrap justify-start lg:justify-between lg:items-center text-white`}
 `;
