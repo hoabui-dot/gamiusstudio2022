3caed2299ebbe7bb9f8c62f045ca289a6fe437de
hoabui-dot <ongchunhostu1998@gmail.com>
2022-05-09 11:12:10 +0700

    remove default code at SectionTitle

--- components/SectionTitle/SectionTitle.tsx
+++ components/SectionTitle/SectionTitle.tsx
@@ -1,24 +1,16 @@
 import React from "react";
-// import * as S from "./Header.styled";
 import * as S from "./SectionTitle.styled"
 
 export interface SectionTitleProps {
   title?: string;
+  subTitle?: string;
 }
 
 export const SectionTitle = (props: SectionTitleProps) => {
   return (
     <S.titleSection>
-        <S.Title>Our Services</S.Title>
-        <S.description>You can find your own solution with our expertise</S.description>
-      {/* <S.FooterContainer>
-        <S.FooterText>This is header</S.FooterText>
-        <S.FooterButton> Click </S.FooterButton>
-
-        <p>
-          toan <span></span>
-        </p>
-      </S.FooterContainer> */}
+        <S.Title>{props.title}</S.Title>
+        <S.description>{props.subTitle}</S.description>
     </S.titleSection>
   );
 };
