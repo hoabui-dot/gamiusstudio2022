e9daa65f008eb38d30c20ce7f9ab34b209b59ace
hoabui-dot <ongchunhostu1998@gmail.com>
2022-05-10 02:40:10 +0700

    create Banner folder

---
+++ components/Banner/Banner.styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+export const Banner = styled.div`
+    ${tw`relative w-full h-[623px]`}
+`
+
+export const Background = styled.div`
+    ${tw`relative w-full h-[632px]`}
+`
