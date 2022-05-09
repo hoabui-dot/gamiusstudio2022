9268d8439d723a4bb25faac2af42d86f2e1fc07f
Trần Hữu Thanh <88789960+IT0505@users.noreply.github.com>
2022-05-09 23:55:22 +0700

    Update Footer

--- components/Footer/components/FooterList/FooterList.tsx
+++ components/Footer/components/FooterList/FooterList.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import * as S from './FooterList.styled';
-import Image from 'next/image';
 
-export interface FooterListProps {}
+interface FooterListProps {}
 
 export const FooterList = (props: FooterListProps) => {
   <S.FooterList></S.FooterList>;
--- utils/dataConfig.ts
+++ utils/dataConfig.ts
@@ -1,7 +1,7 @@
-import ClientSaysImage1 from "../public/img/vn_express.png";
-import ClientSaysImage2 from "../public/img/thanh-nien.png";
-import ClientSaysImage3 from "../public/img/zingnew.png";
-import ClientSaysImage4 from "../public/img/hedima.png";
+import ClientSaysImage1 from '../public/img/vn_express.png';
+import ClientSaysImage2 from '../public/img/thanh-nien.png';
+import ClientSaysImage3 from '../public/img/zingnew.png';
+import ClientSaysImage4 from '../public/img/hedima.png';
 
 export const ClientSaysData = [
   {
