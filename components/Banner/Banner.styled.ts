85ee96bd1e80dbf1441a78c601f89e25512d14e9
hoabui-dot <ongchunhostu1998@gmail.com>
2022-05-10 20:35:28 +0700

    Mockdata in Banner

--- components/Banner/Banner.styled.ts
+++ components/Banner/Banner.styled.ts
@@ -43,7 +43,7 @@ export const BannerImage = styled.div`
 
 
 export const BannerSubTitle = styled.p`
-    ${tw`text-xs xl:text-2xl 2xl:w-[584px] sm:text-sm xl:w-[470px] 
+    ${tw`text-xs xl:text-2xl 2xl:w-[584px] sm:text-sm xl:w-[500px] 
     lg:w-[370px] md:w-[260px] sm:w-[200px] w-[140px] md:text-base md:leading-4 sm:leading-3 font-normal 
     xl:mt-[50px] lg:mt-[25px] sm:mt-[15px] mt-[6px] leading-[11px]`}
 `
@@ -54,18 +54,22 @@ export const GreenColor = styled.span`
 
 export const BannerIcon = styled.div`
     ${tw`absolute xl:w-[110px] lg:w-[70px] xl:bottom-[-13%] lg:bottom-[-16%] 
-    2xl:left-[-15.5%] xl:left-[-19%] lg:left-[-16%] z-50 md:w-[50px] md:left-[-17%] 
+    2xl:left-[-15.5%] xl:left-[-19%] lg:left-[-16%] z-50 md:w-[50px] sm:left-[-17%]
     md:bottom-[-16%] w-[40px] left-[-24%] bottom-[12%]`}
 `
 
 export const BannerStatus = styled.ul`
-    ${tw`absolute flex top-[40%] right-[13%] z-[70]`}
+    ${tw`absolute flex xl:top-[40%] lg:top-[22%]md:top-[-4%] top-[-18px] right-[13%] z-[70]`}
 `
 
 export const Sticked = styled.li`
-    ${tw`relative w-[17px] h-[23px] z-[90]`}
+    ${tw`relative lg:w-[17px] sm:w-[12px] w-[9px] md:w-[14px] h-[23px] z-[90]`}
 `
 
 export const StickedLight = styled.div`
-    ${tw`mt-[-25px]`}
+${tw`mt-[-10px] lg:w-[18px] sm:w-[13px] w-[10px] lg:mt-[-18px] sm:mt-[-12px] md:w-[14px] hidden`}
+
+&.active {
+    ${tw`block`}
+}
 `
