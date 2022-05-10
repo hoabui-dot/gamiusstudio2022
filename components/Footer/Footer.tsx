bd134f63614b3959972626a5c457b83a4f05799a
Trần Hữu Thanh <88789960+IT0505@users.noreply.github.com>
2022-05-10 09:15:01 +0700

    Done Header-Footer(Layout) Desktop

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as S from './Footer.styled';
 import Image from 'next/image';
+import { FooterList } from './components/FooterList/FooterList';
 
 interface FooterProps {}
 
@@ -11,28 +12,36 @@ const MDFooter = {
     phone: '1900-1234',
     openingHours: '8:30 - 20:30 (T2 đến T7)',
   },
-  aboutUs: [
+  listContents: [
     {
-      label: 'Introduce',
-      url: '/',
+      title: 'about us',
+      content: [
+        {
+          label: 'Introduce',
+          url: '/',
+        },
+        {
+          label: 'Contact us',
+          url: '/',
+        },
+      ],
     },
     {
-      label: 'Contact us',
-      url: '/',
-    },
-  ],
-  works: [
-    {
-      label: 'Design 2D',
-      url: '/',
-    },
-    {
-      label: '3d Modeling',
-      url: '/',
-    },
-    {
-      label: 'Videos/VFX',
-      url: '/',
+      title: 'works',
+      content: [
+        {
+          label: 'Design 2D',
+          url: '/',
+        },
+        {
+          label: '3d Modeling',
+          url: '/',
+        },
+        {
+          label: 'Videos/VFX',
+          url: '/',
+        },
+      ],
     },
   ],
 };
@@ -40,8 +49,7 @@ const MDFooter = {
 export const Footer = (props: FooterProps) => {
   const {
     mainInfo: { companyName, location, phone, openingHours },
-    aboutUs,
-    works,
+    listContents,
   } = MDFooter;
   return (
     <S.Footer>
@@ -70,6 +78,11 @@ export const Footer = (props: FooterProps) => {
                 {phone} | {openingHours}
               </S.FooterItem>
             </S.MainInfo>
+            <S.DetailUrl>
+              {listContents.map((item, index) => (
+                <FooterList key={index} data={item} />
+              ))}
+            </S.DetailUrl>
           </S.FooterAboveInner>
         </S.FooterContainer>
       </S.FooterAbove>
--- components/Footer/components/FooterList/FooterList.tsx
+++ components/Footer/components/FooterList/FooterList.tsx
@@ -1,8 +1,29 @@
+import Link from 'next/link';
 import React from 'react';
 import * as S from './FooterList.styled';
 
-interface FooterListProps {}
+interface FooterListProps {
+  data: {
+    title: string;
+    content: {
+      label: string;
+      url: string;
+    }[];
+  };
+}
 
 export const FooterList = (props: FooterListProps) => {
-  <S.FooterList></S.FooterList>;
+  const {
+    data: { title, content },
+  } = props;
+  return (
+    <S.FooterList>
+      <S.ListTitle>{title}</S.ListTitle>
+      {content.map((item, index) => (
+        <Link href={item.url} key={index}>
+          <S.ListItem>{item.label}</S.ListItem>
+        </Link>
+      ))}
+    </S.FooterList>
+  );
 };
