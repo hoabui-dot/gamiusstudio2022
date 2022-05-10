08e7d7d28bb7fdcaa5753310a5e3484abdfef814
hoabui-dot <ongchunhostu1998@gmail.com>
2022-05-10 22:53:04 +0700

    ClientSays responsive

--- components/ClientSays/ClientSays.styled.ts
+++ components/ClientSays/ClientSays.styled.ts
@@ -1,18 +1,18 @@
-import styled from "styled-components";
-import tw from "twin.macro";
+import styled from 'styled-components';
+import tw from 'twin.macro';
 
 export const ClientSays = styled.div`
-    ${tw`flex flex-col justify-center items-center`}
-`
+  ${tw`flex flex-col justify-center items-center`}
+`;
 
 export const Slides = styled.div`
-    ${tw`flex items-center mt-[40px]`}
-`
+  ${tw`flex flex-wrap justify-center items-center mt-[40px]`}
+`;
 
 export const Slide = styled.div`
-    ${tw`h-[54px] w-[160px] relative ml-[60px] first:ml-0`}
-`
+  ${tw`h-[54px] w-[160px] relative ml-[60px] first:ml-0`}
+`;
 
 export const SizeImage = styled.figure`
-    ${tw`h-[32px]`}
-`
+  ${tw`h-[32px]`}
+`;
