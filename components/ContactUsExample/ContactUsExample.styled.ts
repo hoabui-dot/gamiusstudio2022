89618e4c9bafb7fef3afaa1062b26774fa55a3da
Toàn Trần <toantran3110@gmail.com>
2022-05-11 17:35:11 +0700

    Update code handle send email

---
+++ components/ContactUsExample/ContactUsExample.styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+export const ContactUsExampleContainer = styled.div`
+  ${tw`container 
+  mx-auto 
+  px-4 
+  py-8  flex justify-center text-white bg-[#7a7a7a] h-auto `}
+`;
