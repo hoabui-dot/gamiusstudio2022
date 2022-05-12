99ee49bef5e7ca6908e2ced294c85984e3b7081f
hoabui-dot <ongchunhostu1998@gmail.com>
2022-05-12 20:57:50 +0700

    ClientSays responsive

--- components/OurCustomer/OurCustomer.styled.ts
+++ components/OurCustomer/OurCustomer.styled.ts
@@ -1,16 +1,16 @@
 import styled from 'styled-components';
 import tw from 'twin.macro';
 
-export const ClientSays = styled.div`
+export const OurCustomer = styled.div`
   ${tw`flex flex-col justify-center items-center`}
 `;
 
 export const Slides = styled.div`
-    ${tw`flex flex-wrap justify-center items-center mt-[40px]`}
+    ${tw`flex flex-wrap justify-center items-center mt-[40px] mb-[15px]`}
 `
 
 export const Slide = styled.div`
-    ${tw`h-[54px] w-[160px] relative mx-[30px] my-[10px]`}
+    ${tw`h-[50px] w-[134px] relative mx-[30px] my-[10px]`}
 `
 
 export const SizeImage = styled.figure`
