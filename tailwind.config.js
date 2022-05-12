578d932b37e6ca822142c26ab448233b6aee01bf
Trần Hữu Thanh <88789960+IT0505@users.noreply.github.com>
2022-05-12 23:23:47 +0700

    Update background service

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,15 @@ module.exports = {
     extend: {
       keyframes: {
         zoomIn: {
-          '0%': {transform: 'scale(0.5)'},
-          '100%': {transform: 'scale(1)'},
+          '0%': { transform: 'scale(0.5)' },
+          '100%': { transform: 'scale(1)' },
         },
       },
       animation: {
         zoomIn: 'zoomIn 250ms',
-        fadeIn: 'fadeIn 2500ms',
       },
     },
     screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
       xl: '1280px',
       '2xl': '1280px',
     },
