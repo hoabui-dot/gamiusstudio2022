f99ef74155df3e82cc5f49c37dbaa76bb7d5149c
hoabui-dot <ongchunhostu1998@gmail.com>
2022-05-14 17:31:47 +0700

    refactor code

---
+++ components/Approach/components/ApproachItem.styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+export const Item = styled.div`
+  ${tw`text-left border-b-[2px]`}
+`;
+export const ItemTitle = styled.div`
+  ${tw`text-[#C8FC03] mb-[24px] text-2xl font-bold w-[130px]`}
+`;
+export const ItemSubTitle = styled.div`
+  ${tw`text-white mb-[24px] text-base font-normal`}
+`;
