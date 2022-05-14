55171da5d96614a5585cef71723e87a973c9b084
Sangg0708 <ongchunhostu1998@gmail.com>
2022-05-14 22:39:56 +0700

    margin bottom with title and subTitle

--- components/Approach/components/ApproachItem.styled.ts
+++ components/Approach/components/ApproachItem.styled.ts
@@ -5,8 +5,8 @@ export const Item = styled.div`
   ${tw`text-left border-b-[2px]`}
 `;
 export const ItemTitle = styled.div`
-  ${tw`text-green mb-[24px] text-2xl font-bold w-[130px]`}
+  ${tw`text-green mb-[20px] text-2xl font-bold w-[130px]`}
 `;
 export const ItemSubTitle = styled.div`
-  ${tw`text-white mb-[24px] text-base font-normal`}
+  ${tw`text-white mb-[20px] text-base font-normal`}
 `;
