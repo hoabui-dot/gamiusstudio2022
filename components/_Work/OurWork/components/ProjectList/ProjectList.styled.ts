018da4f8baa4035c782825c14c18468d475d63e9
TheHien102 <huynhthehien11102001@gmail.com>
2022-05-16 18:01:39 +0700

    part1-projectList

--- components/_Work/OurWork/components/ProjectList/ProjectList.styled.ts
+++ components/_Work/OurWork/components/ProjectList/ProjectList.styled.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import tw from 'twin.macro';
-
+z;
 export const ProjectList = styled.div`
   ${tw`grid sm:gap-x-12 md:gap-y-0 gap-12 grid-cols-1 md:grid-cols-2 mt-12`}
 `;
