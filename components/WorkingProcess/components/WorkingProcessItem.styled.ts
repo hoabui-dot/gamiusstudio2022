fb4e844b4da8f3dfcc0743428eba45e79008d2a1
hoabui-dot <ongchunhostu1998@gmail.com>
2022-05-16 09:35:46 +0700

    padding subTitle

--- components/WorkingProcess/components/WorkingProcessItem.styled.ts
+++ components/WorkingProcess/components/WorkingProcessItem.styled.ts
@@ -5,14 +5,14 @@ export const Item = styled.div`
   ${tw`flex mb-auto w-[217.6px] flex-col`}
 `;
 export const Image = styled.div`
-  ${tw`w-[207px] mb-[-60px]`}
+  ${tw`w-[207px] mb-[-60px] mt-[-20px]`}
 `;
 export const Content = styled.div`
-  ${tw`px-[20px]`}
+  ${tw`px-[15px]`}
 `;
 export const Title = styled.div`
   ${tw`text-green text-lg text-center font-bold`}
 `;
 export const SubTitle = styled.div`
-  ${tw`text-white text-base leading-4 mt-[16px]`}
+  ${tw`text-white text-[15px] leading-4 mt-[16px]`}
 `;
