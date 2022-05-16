9f019e0e37ec6ab3784517b0bd1a217df9250fb8
hoabui-dot <ongchunhostu1998@gmail.com>
2022-05-16 09:41:29 +0700

    move data into dataConfig

--- components/WorkingProcess/WorkingProcess.tsx
+++ components/WorkingProcess/WorkingProcess.tsx
@@ -3,44 +3,8 @@ import * as S from './WorkingProcess.styled';
 import * as G from '../../styles/global.styled';
 import { SectionTitle } from '../SectionTitle/SectionTitle';
 import { WorkingProcessItem } from './components/WorkingProcessItem';
+import { WorkingProcessData } from "../../utils/dataConfig"
 
-const WorkingProcessData = [
-  {
-    image: '/img/WorkingProcess1.png',
-    subImage: 'Working Process',
-    title: 'Diagnose deeply ',
-    subTitle:
-      'From your desires, we tease out questions, analyse all challenges and insights to find power.',
-  },
-  {
-    image: '/img/WorkingProcess2.png',
-    subImage: 'Working Process',
-    title: 'Build the meaning',
-    subTitle:
-      'All designs deliver deeply meaningful messages as its own value to communicate more effectively.',
-  },
-  {
-    image: '/img/WorkingProcess3.png',
-    subImage: 'Working Process',
-    title: ' Drive memory',
-    subTitle:
-      "Creative designs with iconic identity, sensorial expression attract attention and win customer's headed",
-  },
-  {
-    image: '/img/WorkingProcess4.png',
-    subImage: 'Working Process',
-    title: 'Fine-tune and complete',
-    subTitle:
-      'A finished product always requires a meticulousness in every small detail.',
-  },
-  {
-    image: '/img/WorkingProcess5.png',
-    subImage: 'Working Process',
-    title: 'Aftercare',
-    subTitle:
-      'We provides support throughout and after each project for your greatest satisfaction and experience.',
-  },
-];
 
 export const WorkingProcess = () => {
   return (
@@ -51,7 +15,7 @@ export const WorkingProcess = () => {
           subTitle='Successful process for successful project'
         />
         <S.WorkingProcess>
-          {WorkingProcessData.map((data, index) => (
+          {WorkingProcessData && !!WorkingProcessData.length && WorkingProcessData.map((data, index) => (
             <WorkingProcessItem
               key={index}
               image={data.image}
--- utils/dataConfig.ts
+++ utils/dataConfig.ts
@@ -241,3 +241,41 @@ export const CardsData = [
     ],
   },
 ];
+
+export const WorkingProcessData = [
+  {
+    image: '/img/WorkingProcess1.png',
+    subImage: 'Working Process',
+    title: 'Diagnose deeply ',
+    subTitle:
+      'From your desires, we tease out questions, analyse all challenges and insights to find power.',
+  },
+  {
+    image: '/img/WorkingProcess2.png',
+    subImage: 'Working Process',
+    title: 'Build the meaning',
+    subTitle:
+      'All designs deliver deeply meaningful messages as its own value to communicate more effectively.',
+  },
+  {
+    image: '/img/WorkingProcess3.png',
+    subImage: 'Working Process',
+    title: ' Drive memory',
+    subTitle:
+      "Creative designs with iconic identity, sensorial expression attract attention and win customer's headed",
+  },
+  {
+    image: '/img/WorkingProcess4.png',
+    subImage: 'Working Process',
+    title: 'Fine-tune and complete',
+    subTitle:
+      'A finished product always requires a meticulousness in every small detail.',
+  },
+  {
+    image: '/img/WorkingProcess5.png',
+    subImage: 'Working Process',
+    title: 'Aftercare',
+    subTitle:
+      'We provides support throughout and after each project for your greatest satisfaction and experience.',
+  },
+];
