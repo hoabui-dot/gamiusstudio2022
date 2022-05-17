67b3ce8203aa734755fd0a304a5684eef922e7f3
TheHien102 <huynhthehien11102001@gmail.com>
2022-05-18 00:11:02 +0700

    keyMessage

--- components/KeyMessage/KeyMessage.tsx
+++ components/KeyMessage/KeyMessage.tsx
@@ -6,9 +6,10 @@ import decor from '../../public/img/Decor.png';
 
 export interface KeyMessageProps {
   type?: boolean;
+  story?: boolean;
 }
 
-export const KeyMessage = ({ type }: KeyMessageProps) => {
+export const KeyMessage = ({ type, story }: KeyMessageProps) => {
   return (
     <S.KeyMessage>
       <G.Container>
@@ -16,18 +17,38 @@ export const KeyMessage = ({ type }: KeyMessageProps) => {
           <S.WrapperImage>
             <Image src={decor} alt={'decor'} />
           </S.WrapperImage>
-          {type ? (
-            <S.Text type={true}>
-              <span>Specializing </span>
-              in diversity, we <span> convey meaningful</span> vision through
-              <span> variety of creative </span> and <span> impactful </span>
-              visual solution and <span>support</span> you in your projects.
-            </S.Text>
-          ) : (
-            <S.Text type={false}>
-              We are driven by the purpose of <span>connecting reality </span>
-              to your <span>value </span>
-              and conquering <span> customer’s hearts</span>
+          {!story && (
+            <>
+              {type ? (
+                <S.Text type={true}>
+                  <span>Specializing </span>
+                  in diversity, we <span> convey meaningful</span> vision
+                  through
+                  <span> variety of creative </span> and{' '}
+                  <span> impactful </span>
+                  visual solution and <span>support</span> you in your projects.
+                </S.Text>
+              ) : (
+                <S.Text type={false}>
+                  We are driven by the purpose of{' '}
+                  <span>connecting reality </span>
+                  to your <span>value </span>
+                  and conquering <span> customer’s hearts</span>
+                </S.Text>
+              )}
+            </>
+          )}
+          {story && (
+            <S.Text type={false} story={true}>
+              <span>Gamius </span>
+              is <span>developed </span>with a dimension to
+              <span>create work </span>which is as <span>effective </span>as it
+              is beautiful. We specialize in <span>design </span>and
+              <span>illustraton </span>with a focus on{' '}
+              <span>innovative concepts</span>. We apply a<span>customer</span>{' '}
+              and user centered methodology to gain insights but have great
+              holistic perspective to <span>spark creativity</span>. It is our
+              way to turn <span>ideas into reality</span>.
             </S.Text>
           )}
         </S.WrapperKeyMessage>
