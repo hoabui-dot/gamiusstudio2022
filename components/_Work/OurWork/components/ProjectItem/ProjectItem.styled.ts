af33103b24761f0cbef0aa62dab2b72a851c93d0
Trần Hữu Thanh <88789960+IT0505@users.noreply.github.com>
2022-05-18 21:16:48 +0700

    Update OurWork ModalBox

--- components/_Work/OurWork/components/ProjectItem/ProjectItem.styled.ts
+++ components/_Work/OurWork/components/ProjectItem/ProjectItem.styled.ts
@@ -1,11 +1,7 @@
 import styled from 'styled-components';
 import tw from 'twin.macro';
 
-export const ProjectList = styled.div`
-  ${tw`grid sm:gap-x-12 md:gap-y-0 gap-12 grid-cols-1 md:grid-cols-2 mt-12`}
-`;
-
-export const Project = styled.figure`
+export const ProjectItem = styled.figure`
   ${tw`relative even:mt-0 md:even:mt-12`}
 `;
 
