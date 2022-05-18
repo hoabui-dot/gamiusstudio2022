dec4e9875f606e8a967d014d37e15d45f9cdc52f
Trần Hữu Thanh <88789960+IT0505@users.noreply.github.com>
2022-05-18 23:49:25 +0700

    Update CoreTeam layout

---
+++ components/CoreTeam/CoreTeam.styled.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+export const CoreTeam = styled.div`
+  ${tw`py-[35px]`}
+`;
+
+export const CoreTeamInner = styled.div`
+  ${tw``}
+`;
+
+export const CoreTeamBackground = styled.div`
+  ${tw`bg-core-team bg-center bg-no-repeat bg-cover min-h-[500px]`}
+`;
+
+export const SectionTitle = styled.div`
+  ${tw`my-5 mb-6`}
+`;
+
+export const Title = styled.h2`
+  ${tw`text-4xl font-bold text-green mb-4 text-center`}
+  .whiteTxt {
+    ${tw`text-white`}
+  }
+`;
+
+export const SubTitle = styled.h3`
+  ${tw`text-base font-medium text-white text-center`}
+`;
