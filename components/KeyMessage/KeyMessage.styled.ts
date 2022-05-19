dd1dc49d5d7a275bd8524a6a7c01115edcd3f7ef
Toàn Trần <toantran3110@gmail.com>
2022-05-19 21:27:39 +0700

    update feedback

--- components/KeyMessage/KeyMessage.styled.ts
+++ components/KeyMessage/KeyMessage.styled.ts
@@ -16,13 +16,13 @@ export const WrapperImage = styled.figure`
   ${tw`xl:min-w-[70px] min-w-[45px]`}
 `;
 
-export const Text = styled.div<{ type?: boolean }>`
-  ${({ type }) => {
+export const Text = styled.div<{type?: boolean}>`
+  ${({type}) => {
     return type
       ? tw`xl:text-[30px] text-[20px]`
       : tw`xl:text-[24px] text-[17px]`;
   }}
-  ${tw`mx-auto text-right max-w-[740px] xl:ml-14 ml-7 mt-4 text-white`}
+  ${tw`mx-auto text-right max-w-[740px] xl:ml-14 ml-7 mt-4 text-white font-bold`}
   
   span {
     ${tw`text-[${greenMint}] `}
