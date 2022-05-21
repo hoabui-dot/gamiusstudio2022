f3924d32e806ff6ab367a39f398bdb09bc31be53
Toàn Trần <toantran3110@gmail.com>
2022-05-22 00:36:22 +0700

    Update vercel.sh

--- vercel.sh
+++ vercel.sh
@@ -1,9 +1,14 @@
-if [[ "$VERCEL_GIT_COMMIT_REF" !== "$BRANCH_TO_BUILD" ]] ; then
+#!/bin/bash
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[  "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
   # Proceed with the build
-    echo "✅ - Build can proceed"
+	echo "✅ - Build can proceed"
   exit 1;
+
 else
   # Don't build
-  echo "🛑 - Build canceled"
+  echo "🛑 - Build cancelled"
   exit 0;
 fi
